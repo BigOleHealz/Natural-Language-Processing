95e6744a205ff43b7a00f5901995ef14fe8d0845
Matt Healy <mth47@drexel.edu>
2018-04-02 20:23:46 -0400

    Added Comments, Deleted unneccessary files
    Prints most informative features for all classification algorithms

--- PreprocessingJSON.py
+++ PreprocessingJSON.py
@@ -4,107 +4,87 @@ This script pre-processes all the data from the JSON.
  - Removes stop words
  - Stems words
 '''
-
 import json
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
 import codecs
 
-data_neg = open("twitter_samples/negative_tweets.json", "r")
-data_pos = open("twitter_samples/positive_tweets.json", "r")
+stop_words = set(stopwords.words("english"))	# import most-used words that convey little intent
 
-negative_tweets = codecs.open("twitter_samples/negative_tweets.txt", "w", "utf-8")
-positive_tweets = codecs.open("twitter_samples/positive_tweets.txt", "w", "utf-8")
+ps = PorterStemmer()	# So that we can stem words later (if we want)
 
-stop_words = set(stopwords.words("english"))
-
-ps = PorterStemmer()
-
-def print_info(arg):
-	print("Length:		{}".format(len(arg)))
-	print("Type:		{}".format(type(arg)))
-	print("Value:		{}".format(arg))
-
-def list_to_string(array):
+def list_to_string(array):	# Converts array (list) of words to a string
 	return_string = ""
 
 	for word in array:
 		return_string += word + " "
-	return return_string
+	return return_string	
 
-def need_to_be_altered(array):
+def need_to_be_altered(array):	# Returns true only if the tweet contains a "@blahblah" or a link
 	if "@" in array or "http" in array or "https" in array:
 		return True
 	else:
 		return False
 
-def remove_at(array):
+def remove_at(array):	# Removes "@blahblah"
 	while "@" in array:
-		del array[array.index("@") + 1]
-		del array[array.index("@")]
+		del array[array.index("@") + 1]	# Removes "blahblah"
+		del array[array.index("@")]	# Removes "@"
 
 	return array
 
-def remove_links(array):
+def remove_links(array):	# Removes any link starting with "http" or "https"
 
 	while "http" in array:
-		del array[array.index("http") + 2]
-		del array[array.index("http") + 1]
-		del array[array.index("http")]
+		del array[array.index("http") + 2]	# Removes "//blahblah.com"
+		del array[array.index("http") + 1]	# Removes ":"
+		del array[array.index("http")]	# Removes "http"
 
 	while "https" in array:
- 		del array[array.index("https") + 2]
- 		del array[array.index("https") + 1]
- 		del array[array.index("https")]
+ 		del array[array.index("https") + 2]	# Removes "//blahblah.com"
+ 		del array[array.index("https") + 1]	# Removes ":"
+ 		del array[array.index("https")]	# Removes "https"
 
 	return array
 
-def remove_stop_words(array):
+def remove_stop_words(array):	# Removes most-used words that don't express much opinion like "and", "the", or "in"
 	filtered_array = [word for word in array if not word in stop_words]
 
 	return filtered_array
 
-def stem_words(array):
+def stem_words(array):	# Finds root word of each word in array
 	stemmed_text = [ps.stem(word) for word in array]
 	
 	return stemmed_text
 
-def alter(array):
+def alter(array):	# Removes "@blahblah" and links
 	array = remove_at(array)
 	array = remove_links(array)
 
 	return array
 
-def stop_words_and_stem(string):
-
-	text = string
-	text = remove_stop_words(text)
-	text = list_to_string(text)
-	return text
-
-# Make negative_tweets.txt
-
+# Convert .json to .txt file
 def json_to_txt(file_name):
 	json_file = open("twitter_samples/" + file_name + ".json", "r")
 	tweets = codecs.open("twitter_samples/" + file_name + ".txt", "w", "utf-8")
 
 	for line in json_file:
-		text_arr = [word for word in word_tokenize(json.loads(line)["text"])]
+		text_arr = [word for word in word_tokenize(json.loads(line)["text"])]	# Tokenize each string of words in the "text" field of the json and put the into an array (list)
 
-		if need_to_be_altered(text_arr):
+		if need_to_be_altered(text_arr):	# Removes "@blahblah" and links if need-be
 			text_arr = alter(text_arr)
-		text_arr = remove_stop_words(text_arr)
-		text_arr = stem_words(text_arr)
+		text_arr = remove_stop_words(text_arr)	# Removes stop words
+		# text_arr = stem_words(text_arr)	# Stems all words in the array
 
 		print(list_to_string(text_arr))
 		
 		try:
-			tweets.write(list_to_string(text_arr) + "\n")
+			tweets.write(list_to_string(text_arr) + "\n")	# Cast the list to a string and print it in the "write to" file
 		except:
 			pass
 
 	tweets.close()
 
-json_to_txt("negative_tweets")
-json_to_txt("positive_tweets")
+json_to_txt("negative_tweets")	# Make negative_tweets.txt
+json_to_txt("positive_tweets")	# Make positive_tweets.txt
--- training_new.py
+++ training_new.py
@@ -87,8 +87,8 @@ featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
 random.shuffle(featuresets)
 
-training_set = featuresets[10000:]
-testing_set = featuresets[:10000]
+training_set = featuresets[4500:]
+testing_set = featuresets[:500]
 
 ###############
 classifier = nltk.NaiveBayesClassifier.train(training_set)
@@ -102,6 +102,7 @@ save_classifier.close()
 MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(training_set)
 print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+MNB_classifier.show_most_informative_features(15)
 
 save_classifier = open("pickled_algos/MNB_classifier5k.pickle","wb")
 pickle.dump(MNB_classifier, save_classifier)
@@ -110,6 +111,7 @@ save_classifier.close()
 BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
 BernoulliNB_classifier.train(training_set)
 print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+BernoulliNB_classifier.show_most_informative_features(15)
 
 save_classifier = open("pickled_algos/BernoulliNB_classifier5k.pickle","wb")
 pickle.dump(BernoulliNB_classifier, save_classifier)
@@ -118,6 +120,7 @@ save_classifier.close()
 LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
 print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+LogisticRegression_classifier.show_most_informative_features(15)
 
 save_classifier = open("pickled_algos/LogisticRegression_classifier5k.pickle","wb")
 pickle.dump(LogisticRegression_classifier, save_classifier)
@@ -127,6 +130,7 @@ save_classifier.close()
 LinearSVC_classifier = SklearnClassifier(LinearSVC())
 LinearSVC_classifier.train(training_set)
 print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+LinearSVC_classifier.show_most_informative_features(15)
 
 save_classifier = open("pickled_algos/LinearSVC_classifier5k.pickle","wb")
 pickle.dump(LinearSVC_classifier, save_classifier)
@@ -141,6 +145,7 @@ save_classifier.close()
 SGDC_classifier = SklearnClassifier(SGDClassifier())
 SGDC_classifier.train(training_set)
 print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
+SGDC_classifier.show_most_informative_features(15)
 
 save_classifier = open("pickled_algos/SGDC_classifier5k.pickle","wb")
 pickle.dump(SGDC_classifier, save_classifier)
